b32f6ebcb253ee45450464698d011fd6beee2f13
WillowRussia <iv040606@gmail.com>
2025-10-26 10:16:53 +0300

    Added .gitignore and updated lab setting

--- Lab1/Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
+++ Lab1/Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70876dd1df219e33bb3f4c1cd133d6fcfae568ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
