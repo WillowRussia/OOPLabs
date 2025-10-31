4bc36ab7061e2c7382d98c04656725b3dd51246e
WillowRussia <iv040606@gmail.com>
2025-10-31 16:09:46 +0300

    Added ItemUpgradeService

---
+++ Lab2/Lab2/Modules/ItemUpgradeService.cs
@@ -0,0 +1,16 @@
+namespace RPGInventorySystem.Modules
+{    public class ItemUpgradeService
+    {
+        public Weapon UpgradeWeapon(Weapon weapon, int extraDamage, ItemRarity newRarity)
+        {
+            var newName = weapon.Name + " +1";
+            return new Weapon(newName, weapon.Damage + extraDamage, newRarity);
+        }
+
+        public Armor UpgradeArmor(Armor armor, int extraDefense, ItemRarity newRarity)
+        {
+            var newName = armor.Name + " +1";
+            return new Armor(newName, armor.Defense + extraDefense, newRarity);
+        }
+    }
+}
